3a7bcbc330870845d955bbe6b6891072cce0f539
SilvioBi <biasiol.silvio@gmail.com>
2019-03-04 17:45:45 +0100

    Updates

---
+++ precache-manifest.145cbb73fc7447108f6e33068ab1f6e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b18cbcf55842504f31a3",
+    "url": "/emoji-list/static/js/runtime~main.b18cbcf5.js"
+  },
+  {
+    "revision": "060b9138f13dbbe2fa36",
+    "url": "/emoji-list/static/js/main.060b9138.chunk.js"
+  },
+  {
+    "revision": "8a37065495e0b5150e09",
+    "url": "/emoji-list/static/js/2.8a370654.chunk.js"
+  },
+  {
+    "revision": "060b9138f13dbbe2fa36",
+    "url": "/emoji-list/static/css/main.3ad27aef.chunk.css"
+  },
+  {
+    "revision": "8a37065495e0b5150e09",
+    "url": "/emoji-list/static/css/2.ef3f7427.chunk.css"
+  },
+  {
+    "revision": "fae17eea93bc05e8768c81181ff3427c",
+    "url": "/emoji-list/index.html"
+  }
+];
