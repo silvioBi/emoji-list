636278488c6028f50f0e9598c7b0c7b280548ca0
silvioBi <biasiol.silvio@gmail.com>
2019-04-05 14:41:09 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.unregister();
